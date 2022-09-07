ea154c974234feee35c589567faf94cb0c72ef55
houxinchao <houxinchao@jd.com>
2022-09-07 16:41:44 +0800

    fix: 类型更新

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,10 +1,11 @@
-import type { ReactElement } from 'react'
-export default function Layout(page : ReactElement) {
-  console.log('引用了', page)
+import { DetailedHTMLProps, HTMLAttributes } from "react"
+
+export default function Layout({children} : DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>) {
+  console.log('引用了', children)
     return (
       <div>
         <h1>顶部</h1>
-        <main>{page}</main>
+        <main>{children}</main>
         <footer>dibu</footer>
       </div>
     )
