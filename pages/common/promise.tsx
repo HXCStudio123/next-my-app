7cd1257e3928cc8456369e0c5c10b716fff39644
houxinchao <houxinchao@jd.com>
2022-11-08 18:36:09 +0800

    promise: 补全

--- pages/common/promise.tsx
+++ pages/common/promise.tsx
@@ -2,31 +2,46 @@ import { useRouter } from 'next/router'
 import { MyPromise } from '../util/promise'
 function Common() {
   const mp = new MyPromise((resolve: Function, reject: Function) => {
-    console.log(11)
     // 在此处执行resolve后才算通过
-    setTimeout(() => { resolve(1) }, 100)
+    reject('报错了')
+    // throw new Error('23456789')
+    // setTimeout(() => { resolve(1) }, 100)
   })
-  const a = mp.then((val: MyPromiseResult) => {
-    console.log(2, val)
-    return new MyPromise((r: Function, j: Function) => {
-      r(44444)
-    })
+  const mp1 = new MyPromise((resolve: Function, reject: Function) => {
+    resolve(2)
+    // setTimeout(() => { resolve(1) }, 100)
   })
-  const b = a.then((val: MyPromiseResult) => {
-    console.log(3, val)
-  })
-  const c = b.then((val: MyPromiseResult) => {
-    console.log(4, val)
-  })
-  // 执行回调
-  console.log(33, mp, a, b, c)
+  MyPromise.any([mp, mp1]).then((res: any) => {
+    console.log('结果', res)
+  }, (e: any) => { console.log('抛错', e) })
+  // const a = mp.then((val: MyPromiseResult) => {
+  //   console.log(2, val)
+  //   return new MyPromise((r: Function, j: Function) => {
+  //     r(44444)
+  //   })
+  // })
+  // const b = a.then((val: MyPromiseResult) => {
+  //   console.log(3, val)
+  // })
+  // const c = b.then((val: MyPromiseResult) => {
+  //   console.log(4, val)
+  // })
+  // // 执行回调
+  // console.log(33, mp, a, b, c)
 
   // 比较真实promise
-  // const p = new Promise((resolve: Function, reject: Function) => {
-  //   console.log('promise', 11)
-  //   // resolve(1)
-  //   setTimeout(() => {resolve(1)}, 100)
-  // })
+  const p = new Promise((resolve: Function, reject: Function) => {
+    resolve(1)
+    // setTimeout(() => {resolve(1)}, 100)
+  })
+  console.log(p.finally(()=>console.log('111')))
+  const p1 = new Promise((resolve: Function, reject: Function) => {
+    resolve(2)
+    // setTimeout(() => {resolve(1)}, 100)
+  }).finally((...args)=>{console.log(args)})
+  // Promise.any([p, p1]).then(res => {
+  //   console.log(res)
+  // }).catch(e => console.log('ppp---', e))
   // const a = p.then((val) => {
   //   console.log('promise结果', val)
   //   // return new Promise((r,j)=>{
--- pages/util/promise.ts
+++ pages/util/promise.ts
@@ -15,8 +15,8 @@ class MyPromise {
     try {
       this.initState()
       exec && exec(this._resolve.bind(this), this._reject.bind(this))
-    } catch (e) {
-      console.error(e)
+    } catch (e: any) {
+      this._reject(e)
     }
   }
   private initState() {
@@ -54,14 +54,15 @@ class MyPromise {
               return
             }
             const result = onFullfiled(val)
-            if(result instanceof MyPromise) {
+            if (MyPromise.isPromise(result)) {
               // 返回值是promise
               result.then(resolve, reject)
               return
             }
             resolve(result)
-          } catch (e) {
+          } catch (e: any) {
             reject(e)
+            throw new Error(e)
           }
         }
         timeFn(cb)
@@ -76,8 +77,9 @@ class MyPromise {
             }
             const result = onRejected(reason)
             resolve(result)
-          } catch (e) {
+          } catch (e: any) {
             reject(e)
+            throw new Error(e)
           }
         }
         timeFn(cb)
@@ -93,7 +95,130 @@ class MyPromise {
       }
     })
   }
-  resolve() {}
+  catch(onRejected?: Function) {
+    return this.then(undefined, onRejected)
+  }
+  /**
+   * 无论怎样结果，接收上一个状态的返回值作为入参，并且以该入参为返回值传递下去
+   */
+  finally(cb: Function) {
+    return this.then(
+      (val: MyPromiseResult)=>MyPromise.resolve(cb()).then(()=>val),
+      (reason: MyPromiseResult)=>MyPromise.resolve(cb()).then(()=>{throw reason})
+    )
+  }
+  /**
+   * 成功: 全部成功
+   * 失败: 有一个失败抛错
+   * @return {MyPromise} 返回结果数组
+   */
+  static all(promises: Array<MyPromise>): MyPromise {
+    let count = promises.length
+    let result = new Array(count)
+    return new MyPromise((resolve: Function, reject: Function) => {
+      const add = (res: any, index: number) => {
+        count--
+        result[index] = res
+        if (count === 0) resolve(result)
+      }
+      promises.forEach((promise, index) => {
+        if (MyPromise.isPromise(promise)) {
+          promise.then(
+            (res: MyPromiseResult) => add(res, index),
+            reject
+          )
+        } else {
+          add(promise, index)
+        }
+      })
+    })
+  }
+  /**
+   * 接收一个Promise数组，数组中如有非Promise项，则此项当做成功
+   * 哪个Promise最快得到结果，就返回那个结果，无论成功失败
+   * @param promises 
+   * @returns 
+   */
+  static race(promises: Array<MyPromise>): MyPromise{
+    return new MyPromise((resolve: Function, reject: Function) => {
+      promises.forEach((promise)=>{
+        if(MyPromise.isPromise(promise)) {
+          promise.then(resolve, reject)
+        } else {
+          resolve(promise)
+        }
+      })
+    })
+  }
+  /**
+   * 接收一个Promise数组，数组中如有非Promise项，则此项当做成功
+   * 把每一个Promise的结果，集合成数组，返回
+   * @param promises 
+   * @returns 
+   */
+  static allSettled(promises: Array<MyPromise>): MyPromise{
+    let result = new Array(promises.length)
+    let count = promises.length
+    return new MyPromise((resolve: Function, reject: Function)=>{
+      const add = (status: string, res: any, index: number)=>{
+        count--
+        result[index] = {
+          status,
+          res
+        }
+        if(count === 0) {
+          resolve(result)
+        }
+      }
+      promises.forEach((promise, index)=>{
+        if(MyPromise.isPromise(promise)) {
+          promise.then((res: MyPromiseResult) => add(FULFILLED, res, index), (err: any)=>add(REJECTED, err, index))
+        } else {
+          add(FULFILLED, promise, index)
+        }
+      })
+    })
+  }
+  /**
+   * 接收一个Promise数组，数组中如有非Promise项，则此项当做成功,
+   * 如果有一个Promise成功，则返回这个成功结果
+   * 如果所有Promise都失败，则报错
+   * @param promises 
+   * @returns 
+   */
+  static any(promises: Array<MyPromise>): MyPromise{
+    let count = promises.length
+    return new MyPromise((resolve: Function, reject: Function)=>{
+      const add = ()=>{
+        count--
+        if(count === 0) {
+          reject('所有项均失败')
+        }
+      }
+      promises.forEach((promise, index)=>{
+        if(MyPromise.isPromise(promise)) {
+          promise.then(resolve, (err: any)=> add())
+        } else {
+          resolve(promise)
+        }
+      })
+    })
+  }
+  static isPromise (val: any) {
+    return val && val instanceof MyPromise
+  }
+  static resolve(res: any) {
+    if(MyPromise.isPromise(res)) {
+      return res
+    }
+    return new MyPromise((resolve: Function)=>{
+      if(res && typeof res.then === 'function' ) {
+        res.then(resolve)
+        return
+      }
+      resolve(res)
+    })
+  }
 }
 
 export { MyPromise }
